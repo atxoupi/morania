9fb499abe771c3c2283a546b30872c5ba68b21f0
atxoupi <andres.hermelo@gmail.com>
2022-10-08 17:00:52 +0000

    Deploy Beta version

--- src/front/js/pages/comarcas.js
+++ src/front/js/pages/comarcas.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useContext } from "react";
 import { Link, useParams } from "react-router-dom";
 import { Context } from "../store/appContext";
-//...
+
 export const Comarcas = () => {
   return (
     <div className="container">
