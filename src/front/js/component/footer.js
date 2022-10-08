4409d4086b28a606a7aa825ebcf5fe610abaac75
atxoupi <andres.hermelo@gmail.com>
2022-10-08 18:00:58 +0000

    Derechos...

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -2,6 +2,9 @@ import React, { Component } from "react";
 
 export const Footer = () => (
   <footer className="footer mt-auto py-3 text-center">
-    <p>Morania.gal</p>
+    <p>
+      © 2022 Morania.gal Todos los derechos reservados. <br></br>Sitio web
+      creado por AtxoupiDev by Andrés Hermelo.
+    </p>
   </footer>
 );
